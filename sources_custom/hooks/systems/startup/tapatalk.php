08f0137c51c3ee39791737b105e9e209d22a5901
Chris Graham <chris@ocproducts.com>
2015-09-01 16:46:26 +0100

    Fixes for tapatalk smartbanners

--- sources_custom/hooks/systems/startup/tapatalk.php
+++ sources_custom/hooks/systems/startup/tapatalk.php
@@ -18,7 +18,7 @@
  */
 class Hook_startup_tapatalk
 {
-    function run()
+    public function run()
     {
         if ((get_option('tapatalk_promote_from_website') === '1') && (running_script('index'))) {
             ini_set('ocproducts.type_strictness', '0');
@@ -27,35 +27,47 @@ class Hook_startup_tapatalk
             $app_forum_name = get_site_name();
             $api_key = get_option('tapatalk_api_key');
 
-            $app_location_url = static_evaluate_tempcode(build_url(array('page' => '_SELF'), '_SELF', null, true, false, true));
-            $app_location_url = preg_replace('/^(https|http)/isU', 'tapatalk', $app_location_url);
-
             $app_ads_enable = 0;
             $app_banner_enable = 1;
             $is_mobile_skin = is_mobile() ? 1 : 0;
 
-            process_monikers(get_page_name());
+            process_url_monikers(get_page_name());
 
             $page_type = 'other';
+            $start = mixed();
+            $max = mixed();
+            $extra = '';
             switch (get_page_name()) {
                 case 'topicview':
                     $page_type = 'topic';
+                    $start = get_param_integer('start', 0);
+                    $default_max = intval(get_option('forum_posts_per_page'));
+                    $max = get_param_integer('max', $default_max);
+                    $id = get_param_integer('id', null);
+                    if (!is_null($id)) {
+                        $extra = '&tid=' . strval($id);
+                    }
                     break;
                 case 'forumview':
-                    switch (get_param('type', 'browse')) {
-                        case 'pts':
-                            $page_type = 'pm';
+                    $max = get_param_integer('max', intval(get_option('forum_topics_per_page')));
+                    switch (get_param_string('type', 'browse')) {
+                        case 'pt':
+                            $page_type = 'message';
+                            $start = get_param_integer('start', get_param_integer('kfs', 0));
+                            $id = get_param_integer('id', null);
+                            if (!is_null($id)) {
+                                $extra = '&mid=' . strval($id);
+                            }
                             break;
-                        case 'misc':
-                            if (get_param_integer('id', db_get_first_id()) == db_get_first_id()) {
+                        case 'browse':
+                            $id = get_param_integer('id', db_get_first_id());
+                            if ($id == db_get_first_id()) {
                                 $page_type = 'home';
-
-                                // Less likely to confuse Tapatalks forum detection code
-                                $app_location_url = get_base_url();
-                                $app_location_url = preg_replace('/^(https|http)/isU', 'tapatalk', $app_location_url);
                             } else {
                                 $page_type = 'forum';
+                                $extra = '&fid=' . strval($id);
                             }
+                            $start = get_param_integer('start', get_param_integer('kfs' . strval($id), 0));
                             break;
                     }
                     break;
@@ -64,6 +76,8 @@ class Hook_startup_tapatalk
                     break;
                 case 'members':
                     $page_type = 'profile';
+                    $id = get_param_integer('id', get_member());
+                    $extra = '&uid=' . strval($id);
                     break;
                 case 'users_online':
                     $page_type = 'online';
@@ -73,7 +87,20 @@ class Hook_startup_tapatalk
                     break;
             }
 
+            $app_location_url = get_base_url() . '?';
+            if (!is_guest()) {
+                $app_location_url .= 'user_id=' . strval(get_member()) . '&';
+            }
+            $app_location_url .= 'location=' . $page_type;
+            if (!is_null($start)) {
+                $app_location_url .= '&page=' . strval(intval(floor($start / $max)) + 1) . '&perpage=' . strval($max);
+            }
+            $app_location_url .= $extra;
+            $app_location_url = preg_replace('/^(https|http)/isU', 'tapatalk', $app_location_url);
+
             $tapatalk_dir_url = get_base_url();
+
+            global $app_head_include;
 
             require(get_file_base() . '/mobiquo/smartbanner/head.inc.php');
 
