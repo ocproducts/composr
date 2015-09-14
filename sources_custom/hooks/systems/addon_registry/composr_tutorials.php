be020557fb634b79cd161b396e8af67b0793a904
Chris Graham <chris@ocproducts.com>
2015-09-14 16:59:20 +0100

    More tutorial work

--- sources_custom/hooks/systems/addon_registry/composr_tutorials.php
+++ sources_custom/hooks/systems/addon_registry/composr_tutorials.php
@@ -629,6 +629,8 @@ The documentation is primarily maintained on our website, and this archive is on
             'data_custom/images/docs/tut_short_urls/apache.png',
             'data_custom/images/docs/tut_short_urls/iis.png',
             'data_custom/images/docs/tut_repository/os_webdav.jpg',
+            'data_custom/images/docs/tut_browsers/browsers.png',
+            'data_custom/images/docs/tut_browsers/browserstack.png',
             'data_custom/images/docs/tut_facebook/facebook_addon_a.png',
             'data_custom/images/docs/tut_facebook/facebook_addon_b.png',
             'data_custom/images/docs/tut_facebook/facebook_config.png',
