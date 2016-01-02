9736d0f7bc92dd2428001c78edbf9186c8ae8592
Chris Graham <chris@ocproducts.com>
2016-01-02 23:10:18 +0000

    More installation code

--- data_custom/composr_homesite_install.php
+++ data_custom/composr_homesite_install.php
@@ -46,16 +46,25 @@ require($FILE_BASE . '/sources/global.php');
 // Put code that you temporarily want executed into the function. DELETE THE CODE WHEN YOU'RE DONE.
 // This is useful when performing quick and dirty upgrades (e.g. adding tables to avoid a reinstall)
 
-require_code('database_action');
-require_code('config2');
-require_code('menus2');
-go();
+composr_homesite_install();
 
 /**
  * Execute some install code put into this function.
  */
-function go()
+function composr_homesite_install()
 {
+    global $SITE_INFO;
+    if (get_param_string('password') != $SITE_INFO['master_password']) {
+        warn_exit('Must provide correct master password');
+    }
+
+    header('Content-type: text/plain');
+
+    require_code('database_action');
+    require_code('config2');
+    require_code('menus2');
+    require_code('permissions2');
+
     // NB: The following modules are already covered in the installer...
     //  - docs:tutorials
     //  - adminzone:admin_customers
@@ -92,13 +101,18 @@ function go()
     // -------------------
 
     require_code('downloads2');
-    add_download_category('Releases', db_get_first_id(), "Releases of Composr.\n\nAvoid installing old releases, as they may contain bugs or security holes that we have since fixed.\n\nIf you are trying to upgrade, you\'re looking in the wrong place. Personalised upgrade packages may be generated from the new version announcements in the [page=\"site:news\"]news archive[/page].", '', 'uploads/repimages/installation.png');
-    add_download_category('Addons', db_get_first_id(), "[title=\"2\"]What are addons?[/title]\n\nAddons are new/changed features for Composr. Addons are organised according to the version they are targeted for.\n\nCommunity members are encouraged to submit addons. The addons don\'t need validating, except if you\'re still in the lowest member rank (fan in training). Therefore you should also exercise some caution when installing these third-party addons.\n\n[title=\"2\"]Contributing addons[/title]\n\nBy submitting an addon, you are giving ocProducts permission to redistribute it, but also please consider specifying a licence in your download description.\n\nIf you would like to pass on copyright to ocProducts so that it can be considered for inclusion in a future Composr version, please include this in the \'Notes\' field of the download.\n\nIf you are not attaching a proper Composr addon (an exported TAR file), then please describe how the addon can be installed in the description.\n\n[title=\"2\"]Choose Composr version below[/title]\n", '', 'uploads/repimages/addon.png');
+    $cat_id = add_download_category('Releases', db_get_first_id(), "Releases of Composr.\n\nAvoid installing old releases, as they may contain bugs or security holes that we have since fixed.\n\nIf you are trying to upgrade, you\'re looking in the wrong place. Personalised upgrade packages may be generated from the new version announcements in the [page=\"site:news\"]news archive[/page].", '', 'uploads/repimages/installation.png');
+    set_global_category_access('downloads', $cat_id);
+    $cat_id = add_download_category('Addons', db_get_first_id(), "[title=\"2\"]What are addons?[/title]\n\nAddons are new/changed features for Composr. Addons are organised according to the version they are targeted for.\n\nCommunity members are encouraged to submit addons. The addons don\'t need validating, except if you\'re still in the lowest member rank (fan in training). Therefore you should also exercise some caution when installing these third-party addons.\n\n[title=\"2\"]Contributing addons[/title]\n\nBy submitting an addon, you are giving ocProducts permission to redistribute it, but also please consider specifying a licence in your download description.\n\nIf you would like to pass on copyright to ocProducts so that it can be considered for inclusion in a future Composr version, please include this in the \'Notes\' field of the download.\n\nIf you are not attaching a proper Composr addon (an exported TAR file), then please describe how the addon can be installed in the description.\n\n[title=\"2\"]Choose Composr version below[/title]\n", '', 'uploads/repimages/addon.png');
+    set_global_category_access('downloads', $cat_id);
 
     /* These are added automatically by publish_addons_as_downloads.php scripts, underneath particular Addon release categories
-    add_download_category('Themes', db_get_first_id(), "", '', 'uploads/repimages/xxx');
-    add_download_category('Translations', db_get_first_id(), "", '', 'uploads/repimages/xxx');
-    add_download_category('Addons', db_get_first_id(), "", '', 'uploads/repimages/xxx');
+    $cat_id = add_download_category('Themes', db_get_first_id(), "", '', 'uploads/repimages/xxx');
+    set_global_category_access('downloads', $cat_id);
+    $cat_id = add_download_category('Translations', db_get_first_id(), "", '', 'uploads/repimages/xxx');
+    set_global_category_access('downloads', $cat_id);
+    $cat_id = add_download_category('Addons', db_get_first_id(), "", '', 'uploads/repimages/xxx');
+    set_global_category_access('downloads', $cat_id);
     */
 
     // Catalogues structure
@@ -110,9 +124,12 @@ function go()
     // --------------
 
     require_code('news2');
-    add_news_category('Security issues', 'newscats/difficulties');
-    add_news_category('Announcements', 'newscats/business', '');
-    add_news_category('New releases', 'newscats/new_release', '');
+    $cat_id = add_news_category('Security issues', 'newscats/difficulties', '');
+    set_global_category_access('news', $cat_id);
+    $cat_id = add_news_category('Announcements', 'newscats/business', '');
+    set_global_category_access('news', $cat_id);
+    $cat_id = add_news_category('New releases', 'newscats/new_release', '');
+    set_global_category_access('news', $cat_id);
     delete_news_category(3); // Difficulties
     delete_news_category(5); // Entertainment
     delete_news_category(6); // Business
@@ -130,15 +147,23 @@ function go()
     cns_make_forum_grouping('Composing', "Beyond today\'s Composr (discussion, planning, addons)"); // 3
 
     // Forums
-    cns_make_forum('Introduce yourself', '', 1/*forum grouping*/, null, 1/*parent forum*/, 0/*position*/, 1/*increment*/, 0/*alpha order*/, '', '', '');
+    $cat_id = cns_make_forum('Introduce yourself', '', 1/*forum grouping*/, null, 1/*parent forum*/, 0/*position*/, 1/*increment*/, 0/*alpha order*/, '', '', '');
+    set_global_category_access('forums', $cat_id);
     $deploying_forum_id = cns_make_forum('Deploying', '', 3/*forum grouping*/, null, 1/*parent forum*/, 1/*position*/, 1/*increment*/, 0/*alpha order*/, '', '', '');
-    cns_make_forum('Designing', '', 3/*forum grouping*/, null, 1/*parent forum*/, 2/*position*/, 1/*increment*/, 0/*alpha order*/, '', '', '');
-    cns_make_forum('Developing', '', 3/*forum grouping*/, null, 1/*parent forum*/, 3/*position*/, 1/*increment*/, 0/*alpha order*/, '', '', '');
-    cns_make_forum('Addons', '', 3/*forum grouping*/, null, 1/*parent forum*/, 4/*position*/, 1/*increment*/, 0/*alpha order*/, '', '', '');
-    cns_make_forum('Internationalisation', '', 3/*forum grouping*/, null, 1/*parent forum*/, 5/*position*/, 1/*increment*/, 0/*alpha order*/, '', '', '');
-    cns_make_forum('Feature requests and bug tracking', '', 3/*forum grouping*/, null, $deploying_forum_id/*parent forum*/, 1/*position*/, 1/*increment*/, 0/*alpha order*/, '', '', 'tracker');
-    cns_make_forum('FAQ', '', 3/*forum grouping*/, null, $deploying_forum_id/*parent forum*/, 2/*position*/, 1/*increment*/, 0/*alpha order*/, '', '', 'docs/faq.htm');
-    cns_make_forum('Paid support', '', 3/*forum grouping*/, null, $deploying_forum_id/*parent forum*/, 3/*position*/, 1/*increment*/, 0/*alpha order*/, '', '', 'professional_support.htm');
+    $cat_id = cns_make_forum('Designing', '', 3/*forum grouping*/, null, 1/*parent forum*/, 2/*position*/, 1/*increment*/, 0/*alpha order*/, '', '', '');
+    set_global_category_access('forums', $cat_id);
+    $cat_id = cns_make_forum('Developing', '', 3/*forum grouping*/, null, 1/*parent forum*/, 3/*position*/, 1/*increment*/, 0/*alpha order*/, '', '', '');
+    set_global_category_access('forums', $cat_id);
+    $cat_id = cns_make_forum('Addons', '', 3/*forum grouping*/, null, 1/*parent forum*/, 4/*position*/, 1/*increment*/, 0/*alpha order*/, '', '', '');
+    set_global_category_access('forums', $cat_id);
+    $cat_id = cns_make_forum('Internationalisation', '', 3/*forum grouping*/, null, 1/*parent forum*/, 5/*position*/, 1/*increment*/, 0/*alpha order*/, '', '', '');
+    set_global_category_access('forums', $cat_id);
+    $cat_id = cns_make_forum('Feature requests and bug tracking', '', 3/*forum grouping*/, null, $deploying_forum_id/*parent forum*/, 1/*position*/, 1/*increment*/, 0/*alpha order*/, '', '', 'tracker');
+    set_global_category_access('forums', $cat_id);
+    $cat_id = cns_make_forum('FAQ', '', 3/*forum grouping*/, null, $deploying_forum_id/*parent forum*/, 2/*position*/, 1/*increment*/, 0/*alpha order*/, '', '', 'docs/faq.htm');
+    set_global_category_access('forums', $cat_id);
+    $cat_id = cns_make_forum('Paid support', '', 3/*forum grouping*/, null, $deploying_forum_id/*parent forum*/, 3/*position*/, 1/*increment*/, 0/*alpha order*/, '', '', 'professional_support.htm');
+    set_global_category_access('forums', $cat_id);
 
     // Theme
     // -----
@@ -149,4 +174,20 @@ function go()
     // ----
 
     import_menu_csv(get_custom_file_base() . '/uploads/website_specific/cms_menu_items.csv');
+
+    // Permissions
+    // -----------
+
+    $GLOBALS['SITE_DB']->query_delete('group_zone_access', array('zone_name' => 'cms'));
+    $GLOBALS['SITE_DB']->query_delete('group_zone_access', array('zone_name' => 'collaboration'));
+
+    $groups = $GLOBALS['SITE_DB']->query_select('group_zone_access', array('group_id'), array('zone_name' => ''));
+    foreach (array('forum', 'site', 'docs', 'ocproducts') as $zone) {
+        $GLOBALS['SITE_DB']->query_delete('group_zone_access', array('zone_name' => $zone));
+        foreach ($groups as $group) {
+            $GLOBALS['SITE_DB']->query_insert('group_zone_access', array('zone_name' => $zone, 'group_id' => $group['group_id']));
+        }
+    }
+
+    echo 'Done';
 }
