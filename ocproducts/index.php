1ffa945cd310d4fba3220cb1acd285084935a3ec
Chris Graham <chris@ocproducts.com>
2016-04-23 09:20:20 +0100

    Adding missing index.php for ocproducts zone

---
+++ ocproducts/index.php
@@ -0,0 +1,56 @@
+<?php /*
+
+ Composr
+ Copyright (c) ocProducts, 2004-2016
+
+ See text/EN/licence.txt for full licencing information.
+
+*/
+
+/**
+ * @license    http://opensource.org/licenses/cpal_1.0 Common Public Attribution License
+ * @copyright  ocProducts Ltd
+ * @package    core
+ */
+
+// This is the standard zone bootstrap file. Zone: site.
+
+// Find Composr base directory, and chdir into it
+global $FILE_BASE, $RELATIVE_PATH;
+$FILE_BASE = (strpos(__FILE__, './') === false) ? __FILE__ : realpath(__FILE__);
+$FILE_BASE = dirname($FILE_BASE);
+if (!is_file($FILE_BASE . '/sources/global.php')) {
+    $RELATIVE_PATH = basename($FILE_BASE);
+    $FILE_BASE = dirname($FILE_BASE);
+} else {
+    $RELATIVE_PATH = '';
+}
+if (!is_file($FILE_BASE . '/sources/global.php')) {
+    $FILE_BASE = $_SERVER['SCRIPT_FILENAME']; // this is with symlinks-unresolved (__FILE__ has them resolved); we need as we may want to allow zones to be symlinked into the base directory without getting path-resolved
+    $FILE_BASE = dirname($FILE_BASE);
+    if (!is_file($FILE_BASE . '/sources/global.php')) {
+        $RELATIVE_PATH = basename($FILE_BASE);
+        $FILE_BASE = dirname($FILE_BASE);
+    } else {
+        $RELATIVE_PATH = '';
+    }
+}
+@chdir($FILE_BASE);
+
+global $FORCE_INVISIBLE_GUEST;
+$FORCE_INVISIBLE_GUEST = false;
+global $EXTERNAL_CALL;
+$EXTERNAL_CALL = false;
+global $CSRF_TOKENS;
+$CSRF_TOKENS = true;
+global $STATIC_CACHE_ENABLED;
+$STATIC_CACHE_ENABLED = true;
+global $IN_SELF_ROUTING_SCRIPT;
+$IN_SELF_ROUTING_SCRIPT = true;
+if (!is_file($FILE_BASE . '/sources/global.php')) {
+    exit('<!DOCTYPE html>' . "\n" . '<html lang="EN"><head><title>Critical startup error</title></head><body><h1>Composr startup error</h1><p>The second most basic Composr startup file, sources/global.php, could not be located. This is almost always due to an incomplete upload of the Composr system, so please check all files are uploaded correctly.</p><p>Once all Composr files are in place, Composr must actually be installed by running the installer. You must be seeing this message either because your system has become corrupt since installation, or because you have uploaded some but not all files from our manual installer package: the quick installer is easier, so you might consider using that instead.</p><p>ocProducts maintains full documentation for all procedures and tools, especially those for installation. These may be found on the <a href="http://compo.sr">Composr website</a>. If you are unable to easily solve this problem, we may be contacted from our website and can help resolve it for you.</p><hr /><p style="font-size: 0.8em">Composr is a website engine created by ocProducts.</p></body></html>');
+}
+require($FILE_BASE . '/sources/global.php');
+
+// If we're still here, we're ok to go
+do_site();
