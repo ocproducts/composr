27c24fdd477b5e6d192ba7bcbab208acf5569830
Chris Graham <chris@ocproducts.com>
2016-06-16 14:10:58 +0100

    Fix IIS incompatibility

--- sources/users.php
+++ sources/users.php
@@ -93,13 +93,13 @@ function init__users()
     }
 
     // Canonicalise various disparities in how HTTP auth environment variables are set
-    if (array_key_exists('REDIRECT_REMOTE_USER', $_SERVER)) {
+    if (!empty($_SERVER['REDIRECT_REMOTE_USER'])) {
         $_SERVER['PHP_AUTH_USER'] = preg_replace('#@.*$#', '', $_SERVER['REDIRECT_REMOTE_USER']);
     }
-    if (array_key_exists('PHP_AUTH_USER', $_SERVER)) {
+    if (!empty($_SERVER['PHP_AUTH_USER'])) {
         $_SERVER['PHP_AUTH_USER'] = preg_replace('#@.*$#', '', $_SERVER['PHP_AUTH_USER']);
     }
-    if (array_key_exists('REMOTE_USER', $_SERVER)) {
+    if (!empty($_SERVER['REMOTE_USER'])) {
         $_SERVER['PHP_AUTH_USER'] = preg_replace('#@.*$#', '', $_SERVER['REMOTE_USER']);
     }
 
@@ -443,7 +443,7 @@ function is_httpauth_login()
     }
 
     require_code('cns_members');
-    return ((array_key_exists('PHP_AUTH_USER', $_SERVER)) && (!is_null(cns_authusername_is_bound_via_httpauth($_SERVER['PHP_AUTH_USER']))));
+    return ((!empty($_SERVER['PHP_AUTH_USER'])) && (!is_null(cns_authusername_is_bound_via_httpauth($_SERVER['PHP_AUTH_USER']))));
 }
 
 /**
