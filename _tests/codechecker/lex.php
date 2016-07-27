40abce2f8088e86d1de893cdca4d8e17456345ca
Chris Graham <chris@ocproducts.com>
2016-07-27 09:20:59 +0100

    Starting to work on namespace support

--- _tests/codechecker/lex.php
+++ _tests/codechecker/lex.php
@@ -95,8 +95,10 @@ $PTOKENS['MULTIPLY'] = '*';
 $PTOKENS['EXPONENTIATION'] = '**';
 $PTOKENS['DIVIDE'] = '/';
 $PTOKENS['REMAINDER'] = '%';
-// Classes/objects
+// Namespaces
+$PTOKENS['NAMESPACE'] = 'namespace';
 $PTOKENS['USE'] = 'use';
+// Classes/objects
 $PTOKENS['INSTEADOF'] = 'insteadof';
 $PTOKENS['SCOPE'] = '::';
 $PTOKENS['CLASS'] = 'class';
@@ -163,7 +165,7 @@ global $PCONTINUATIONS;
 $PCONTINUATIONS = array(
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
     'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-    '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '_');
+    '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '_', '\\');
 global $PCONTINUATIONS_SIMPLE;
 $PCONTINUATIONS_SIMPLE = array(
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
