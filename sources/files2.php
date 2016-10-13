a1e4e3e064daf64e61885720d2bc2ba4230bcd9c
Chris Graham <chris@ocproducts.com>
2016-10-13 11:13:07 -0500

    Misc fixes

--- sources/files2.php
+++ sources/files2.php
@@ -2035,14 +2035,13 @@ function get_webpage_meta_details($url)
             $meta_details['t_image_url'] = qualify_url($meta_details['t_image_url'], $url);
         }
 
-        global $HTTP_DOWNLOAD_MIME_TYPE;
-        if (($HTTP_DOWNLOAD_MIME_TYPE == 'application/octet-stream') || ($HTTP_DOWNLOAD_MIME_TYPE == '')) {
+        if (($result[1] == 'application/octet-stream') || ($result[1] == '')) {
             // Lame, no real mime type - maybe the server is just not configured to know it - try and guess by using the file extension and our own Composr list
             require_code('mime_types');
             require_code('files');
             $meta_details['t_mime_type'] = get_mime_type(get_file_extension($url), true);
         } else {
-            $meta_details['t_mime_type'] = $HTTP_DOWNLOAD_MIME_TYPE;
+            $meta_details['t_mime_type'] = $result[1];
         }
 
         $matches = array();
