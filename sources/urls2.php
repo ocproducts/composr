0704e97a312ba2d0c2ab541eec89ecd6e3454d5e
Chris Graham <chris@ocproducts.com>
2016-12-10 21:38:15 +0000

    Misc fixes and tweaks

--- sources/urls2.php
+++ sources/urls2.php
@@ -793,11 +793,23 @@ function _choose_moniker($page, $type, $id, $moniker_src, $no_exists_check_for =
  */
 function _generate_moniker($moniker_src)
 {
-    $moniker_src = strip_comcode($moniker_src);
+    $moniker = strip_comcode($moniker_src);
 
     $max_moniker_length = intval(get_option('max_moniker_length'));
 
-    $moniker = str_replace(array('ä', 'ö', 'ü', 'ß'), array('ae', 'oe', 'ue', 'ss'), $moniker_src);
+    // Transliteration first
+    if (get_charset() == 'utf-8') {
+        if (function_exists('transliterator_transliterate')) {
+            $moniker = transliterator_transliterate('Any-Latin; Latin-ASCII; Lower()', $moniker);
+        } elseif ((function_exists('iconv')) && (get_value('disable_iconv') !== '1')) {
+            $moniker = iconv('utf-8', 'ASCII//TRANSLIT//IGNORE', $moniker);
+        } else {
+            // German has inbuilt transliteration
+            $moniker = str_replace(array('ä', 'ö', 'ü', 'ß'), array('ae', 'oe', 'ue', 'ss'), $moniker);
+        }
+    }
+
+    // Then strip down / substitute to force it to be URL-ready
     $moniker = str_replace("'", '', $moniker);
     $moniker = strtolower(preg_replace('#[^A-Za-z\d\-]#', '-', $moniker));
     if (strlen($moniker) > $max_moniker_length) {
@@ -809,6 +821,8 @@ function _generate_moniker($moniker_src)
     }
     $moniker = preg_replace('#\-+#', '-', $moniker);
     $moniker = rtrim($moniker, '-');
+
+    // A bit lame, but maybe we'll have to
     if ($moniker == '') {
         $moniker = 'untitled';
     }
