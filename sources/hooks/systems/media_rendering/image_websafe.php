49ee38c586303389f5b0c404b06a7703fbaf51dd
Chris Graham <chris@ocproducts.com>
2017-01-12 23:06:58 +0000

    Do not autodetect width and height for images needlessly, breaks max-width capability

--- sources/hooks/systems/media_rendering/image_websafe.php
+++ sources/hooks/systems/media_rendering/image_websafe.php
@@ -138,6 +138,6 @@ class Hook_media_rendering_image_websafe
             $url_direct_filesystem = get_custom_base_url() . '/' . $url_direct_filesystem;
         }
 
-        return do_template('MEDIA_IMAGE_WEBSAFE', array('_GUID' => '4dbc2c00dd049f9951c27d198065a4c2', 'URL_SAFE' => $_url_safe, 'URL_DIRECT_FILESYSTEM' => $url_direct_filesystem, 'HOOK' => 'image_websafe') + _create_media_template_parameters($url, $attributes, $as_admin, $source_member));
+        return do_template('MEDIA_IMAGE_WEBSAFE', array('_GUID' => '4dbc2c00dd049f9951c27d198065a4c2', 'URL_SAFE' => $_url_safe, 'URL_DIRECT_FILESYSTEM' => $url_direct_filesystem, 'HOOK' => 'image_websafe') + _create_media_template_parameters($url, $attributes, $as_admin, $source_member, true));
     }
 }
