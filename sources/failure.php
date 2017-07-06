d20faa9c80f91505f5eeb7af20e6221011f6c047
Chris Graham <chris@ocproducts.com>
2017-07-06 23:49:38 +0100

    No error mails about taxcloud expiring

--- sources/failure.php
+++ sources/failure.php
@@ -1100,6 +1100,7 @@ function relay_error_notification($text, $ocproducts = true, $notification_type 
         (strpos($text, 'Access denied for') === false) &&
         (strpos($text, 'Unknown database') === false) &&
         (strpos($text, 'headers already sent') === false) &&
+        (strpos($text, 'Your TaxCloud API trial period has expired') === false) &&
         (strpos($text, 'Broken pipe') === false) &&
         (preg_match('#php\.net.*SSL3_GET_SERVER_CERTIFICATE:certificate #', $text) == 0) && // Missing certificates on server
         (preg_match('#Maximum execution time of \d+ seconds#', $text) == 0) &&
