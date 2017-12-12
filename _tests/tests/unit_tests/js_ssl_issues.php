5b6066f1633525e398e1d4970e5f46506e45f3b6
Salman Abbas <salmanabbas007@gmail.com>
2017-12-12 14:36:53 +0500

    Fixed the js_ssl_issues unit test

--- _tests/tests/unit_tests/js_ssl_issues.php
+++ _tests/tests/unit_tests/js_ssl_issues.php
@@ -27,7 +27,7 @@ class js_ssl_issues_test_set extends cms_test_case
                 $file = file_get_contents($path . '/' . $f);
 
                 $matches = array();
-                $num_matches = preg_match_all('#(?<!\$cms\.img\([\'"])\{\$IMG[;*]+,(\w+)\}(.*)$#m', $file, $matches);
+                $num_matches = preg_match_all('#(?<!\$util\.srl\([\'"])\{\$IMG[;*]+,(\w+)\}(.*)$#m', $file, $matches);
                 for ($i = 0; $i < $num_matches; $i++) {
                     $this->assertTrue(false, $f . '/' . $matches[1][$i] . ' not prepared for SSL');
                 }
