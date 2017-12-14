1c294e8eb22be09262016a2255a972fe36907a96
World Wide Web Server <_www@Chriss-iMac.local>
2017-12-14 20:44:16 +0000

    Committed fix to issue #3449 (http://compo.sr/tracker/view.php?id=3449). [Error saving profiles if using the Facebook addon]

--- sources/cns_members_action2.php
+++ sources/cns_members_action2.php
@@ -451,14 +451,8 @@ function cns_get_member_fields_settings($mini_mode = true, $member_id = null, $g
     }
     require_code('form_templates');
     require_code('encryption');
-    if (($special_type == '') && (!is_null($member_id))) {
-        if (cns_is_ldap_member($member_id)) {
-            $special_type = 'ldap';
-        }
-        if (cns_is_httpauth_member($member_id)) {
-            $special_type = 'httpauth';
-        }
-        $special_type = $GLOBALS['FORUM_DRIVER']->get_member_row_field($member_id, 'm_password_compat_scheme');
+    if (($special_type == '') && ($member_id !== null)) {
+        $special_type = get_member_special_type($member_id);
     }
 
     if (is_null($groups)) {
