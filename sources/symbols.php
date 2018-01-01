21c1bcae3196f200a6bb8f06aaef5a98894c4e4b
Chris Graham <chris@ocproducts.com>
2018-01-01 12:37:18 -0600

    Fixed copyright transition issue

--- sources/symbols.php
+++ sources/symbols.php
@@ -4804,3 +4804,27 @@ function ecv_LENGTH($lang, $escaped, $param)
     }
     return $value;
 }
+
+/**
+ * Evaluate a particular Tempcode symbol.
+ *
+ * @ignore
+ *
+ * @param  LANGUAGE_NAME $lang The language to evaluate this symbol in (some symbols refer to language elements).
+ * @param  array $escaped Array of escaping operations.
+ * @param  array $param Parameters to the symbol. For all but directive it is an array of strings. For directives it is an array of Tempcode objects. Actually there may be template-style parameters in here, as an influence of singular_bind and these may be Tempcode, but we ignore them.
+ * @return string The result.
+ */
+function ecv_COPYRIGHT($lang, $escaped, $param)
+{
+    $value = get_option('copyright');
+    if (strpos($CURRENT_YEAR, '$') !== false) {
+        $value = str_replace('$CURRENT_YEAR=', '', $value); // Update-on-posting, does nothing dynamically
+        $value = str_replace('$CURRENT_YEAR', date('Y'), $value); // Always updated
+    }
+
+    if ($escaped !== array()) {
+        apply_tempcode_escaping($escaped, $value);
+    }
+    return $value;
+}
