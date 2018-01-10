7884bdb7072fe2387d98e02508ab9ad9ae21a775
Chris Graham <chris@ocproducts.com>
2018-01-10 02:11:52 +0000

    Updated copyright date

--- mobiquo/mbqClass/read/pm_read.php
+++ mobiquo/mbqClass/read/pm_read.php
@@ -1,7 +1,7 @@
 ptrad<?php /*
 
  Composr
- Copyright (c) ocProducts, 2004-2017
+ Copyright (c) ocProducts, 2004-2018
 
  See text/EN/licence.txt for full licencing information.
 
--- sources_custom/transifex.php
+++ sources_custom/transifex.php
@@ -1,7 +1,7 @@
 <?php /*
 
  Composr
- Copyright (c) ocProducts, 2004-2017
+ Copyright (c) ocProducts, 2004-2018
 
  See text/EN/licence.txt for full licencing information.
 
@@ -613,7 +613,7 @@ Translations may also be downloaded directly from Transifex.";
 {$open} /*
 
  Composr
- Copyright (c) ocProducts, 2004-2017
+ Copyright (c) ocProducts, 2004-2018
 
  See text/EN/licence.txt for full licencing information.
 
