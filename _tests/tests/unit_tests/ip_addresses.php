242c69dcf6242b5d24b0ea3cca195c797b419219
Chris Graham <chris@ocproducts.com>
2018-01-26 00:20:32 +0000

    CIDR tested

---
+++ _tests/tests/unit_tests/ip_addresses.php
@@ -0,0 +1,26 @@
+<?php /*
+
+ Composr
+ Copyright (c) ocProducts, 2004-2018
+
+ See text/EN/licence.txt for full licencing information.
+
+*/
+
+/**
+ * @license    http://opensource.org/licenses/cpal_1.0 Common Public Attribution License
+ * @copyright  ocProducts Ltd
+ * @package    testing_platform
+ */
+
+/**
+ * Composr test case class (unit testing).
+ */
+class ip_addresses_test_set extends cms_test_case
+{
+    public function testCIDR()
+    {
+        $this->assertTrue(ip_cidr_check('204.93.240.1', '204.93.240.0/24'));
+        $this->assertTrue(!ip_cidr_check('204.93.241.1', '204.93.240.0/24'));
+    }
+}
