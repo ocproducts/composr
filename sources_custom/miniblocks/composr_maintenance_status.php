5c52115cc1e9ff34201917626ff79ee0f39de8c2
Chris Graham <chris@ocproducts.com>
2018-02-01 17:11:14 +0000

    Misc fixes around maintenance and test code verification

--- sources_custom/miniblocks/composr_maintenance_status.php
+++ sources_custom/miniblocks/composr_maintenance_status.php
@@ -2,7 +2,7 @@
 
 i_solemnly_declare(I_UNDERSTAND_SQL_INJECTION | I_UNDERSTAND_XSS | I_UNDERSTAND_PATH_INJECTION);
 
-$myfile = fopen(get_custom_file_base() . '/data/maintenance_status.csv', 'rb');
+$myfile = fopen(get_file_base() . '/data/maintenance_status.csv', 'rb');
 
 $header_row = fgetcsv($myfile); // Header row
 unset($header_row[0]);
