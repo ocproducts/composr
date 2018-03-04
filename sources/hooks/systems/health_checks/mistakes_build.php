3ad479f2126b0b0aef0ed4ce798e5604d0e08510
Chris Graham <chris@ocproducts.com>
2018-03-04 23:28:52 +0000

    Giving Firefox some more love

--- sources/hooks/systems/health_checks/mistakes_build.php
+++ sources/hooks/systems/health_checks/mistakes_build.php
@@ -80,7 +80,7 @@ class Hook_health_check_mistakes_build extends Hook_Health_Check
         $this->stateCheckManual('Do a [url="general check"]https://www.woorank.com/[/url] (take warnings with a pinch of salt, not every suggestion is appropriate)');
         $this->stateCheckManual('Do a [url="general check"]https://website.grader.com/[/url] (take warnings with a pinch of salt, not every suggestion is appropriate)');
 
-        $this->stateCheckManual('Test in Firefox');
+        $this->stateCheckManual('Test in Mozilla Firefox');
         $this->stateCheckManual('Test in Google Chrome');
         $this->stateCheckManual('Test in IE10');
         $this->stateCheckManual('Test in IE11');
