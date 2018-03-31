b22edd790516ea1d72aaa5e148c821c17bdca15f
Chris Graham <chris@ocproducts.com>
2018-03-31 05:21:05 +0100

    More protection for PHP Windows filenaming bug with non-latin characters

--- adminzone/pages/modules/admin_themes.php
+++ adminzone/pages/modules/admin_themes.php
@@ -740,6 +740,12 @@ class Module_admin_themes
         $theme = post_param_string('theme');
         require_code('type_sanitisation');
         if (!is_alphanumeric($theme)) {
+            if ((stripos(PHP_OS, 'WIN') === 0) && (version_compare(PHP_VERSION, '7.2', '<'))) {
+                // Older versions of PHP on Windows cannot handle utf-8 filenames
+                require_code('character_sets');
+                $theme = transliterate_string($theme);
+            }
+
             $theme = preg_replace('#[^' . URL_CONTENT_REGEXP . ']#', '_', $theme);
             //warn_exit(do_lang_tempcode('BAD_CODENAME'));
         }
