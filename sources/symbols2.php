834bcffe8fb335cc702597f234821d71eeb4e1e0
Salman Abbas <salmanabbas007@gmail.com>
2018-04-18 17:40:31 +0500

    Implemented {$STARTS_WITH} and {$ENDS_WITH}

--- sources/symbols2.php
+++ sources/symbols2.php
@@ -2154,6 +2154,56 @@ function ecv2_STRPOS($lang, $escaped, $param)
  * @param  array $param Parameters to the symbol. For all but directive it is an array of strings. For directives it is an array of Tempcode objects. Actually there may be template-style parameters in here, as an influence of singular_bind and these may be Tempcode, but we ignore them.
  * @return string The result
  */
+function ecv2_STARTS_WITH($lang, $escaped, $param)
+{
+    $value = '1';
+
+    if (isset($param[1]) && ($param[1] !== '')) {
+        $value = substr($param[0], 0, strlen($param[1])) === $param[1] ? '1' : '0';
+    }
+
+    if ($GLOBALS['XSS_DETECT']) {
+        ocp_mark_as_escaped($value);
+    }
+
+    return $value;
+}
+
+/**
+ * Evaluate a particular Tempcode symbol.
+ *
+ * @ignore
+ *
+ * @param  LANGUAGE_NAME $lang The language to evaluate this symbol in (some symbols refer to language elements)
+ * @param  array $escaped Array of escaping operations
+ * @param  array $param Parameters to the symbol. For all but directive it is an array of strings. For directives it is an array of Tempcode objects. Actually there may be template-style parameters in here, as an influence of singular_bind and these may be Tempcode, but we ignore them.
+ * @return string The result
+ */
+function ecv2_ENDS_WITH($lang, $escaped, $param)
+{
+    $value = '1';
+
+    if (isset($param[1]) && ($param[1] !== '')) {
+        $value = substr($param[0], -strlen($param[1])) === $param[1] ? '1' : '0';
+    }
+
+    if ($GLOBALS['XSS_DETECT']) {
+        ocp_mark_as_escaped($value);
+    }
+
+    return $value;
+}
+
+/**
+ * Evaluate a particular Tempcode symbol.
+ *
+ * @ignore
+ *
+ * @param  LANGUAGE_NAME $lang The language to evaluate this symbol in (some symbols refer to language elements)
+ * @param  array $escaped Array of escaping operations
+ * @param  array $param Parameters to the symbol. For all but directive it is an array of strings. For directives it is an array of Tempcode objects. Actually there may be template-style parameters in here, as an influence of singular_bind and these may be Tempcode, but we ignore them.
+ * @return string The result
+ */
 function ecv2_SUBTRACT($lang, $escaped, $param)
 {
     $value = '';
