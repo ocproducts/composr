e67ce1b462617bc009a0ad6e8548691c8c6c6e70
Salman Abbas <salmanabbas007@gmail.com>
2018-06-12 19:22:50 +0500

    Added {$COLOR_RGBA}

--- sources/symbols2.php
+++ sources/symbols2.php
@@ -3002,3 +3002,30 @@ function ecv2_WHILE(&$value, $lang, $escaped, $param)
         }
     }
 }
+
+/**
+ * Evaluate a particular Tempcode directive.
+ *
+ * @ignore
+ *
+ * @param  LANGUAGE_NAME $lang The language to evaluate this symbol in (some symbols refer to language elements)
+ * @param  array $escaped Array of escaping operations
+ * @param  array $param Parameters to the symbol. For all but directive it is an array of strings. For directives it is an array of Tempcode objects. Actually there may be template-style parameters in here, as an influence of singular_bind and these may be Tempcode, but we ignore them.
+ * @return string
+ */
+function ecv2_COLOR_RGBA($lang, $escaped, $param)
+{
+    $hex_color = $param[0];
+    $red   = hexdec(substr($hex_color, 1, 2));
+    $green = hexdec(substr($hex_color, 3, 2));
+    $blue  = hexdec(substr($hex_color, 5, 2));
+    $alpha = floatval($param[1]);
+
+    $value = "rgba({$red}, {$green}, {$blue}, {$alpha})";
+
+    if ($escaped !== array()) {
+        apply_tempcode_escaping($escaped, $value);
+    }
+
+    return $value;
+}
