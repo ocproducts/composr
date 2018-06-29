472dcd4a772af2dc9a1909707976af267af99d47
Chris Graham <chris@ocproducts.com>
2018-06-29 12:07:39 -0500

    Workaround for Google indexing post_comment.php (it finds it via our JS code)

--- sources/global2.php
+++ sources/global2.php
@@ -649,6 +649,8 @@ function memory_tracking()
  */
 function prepare_for_known_ajax_response()
 {
+    header('X-Robots-Tag: noindex');
+
     header('Cache-Control: no-cache, must-revalidate'); // HTTP/1.1
     header('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
 
