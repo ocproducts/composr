8d6cb921ec2dbd77eaccaf692992cfcc33c43914
Salman Abbas <salmanabbas007@gmail.com>
2018-08-05 08:43:37 +0500

    Workaround for Xdebug bug #1556 https://bugs.xdebug.org/view.php?id=1556

--- sources/tasks.php
+++ sources/tasks.php
@@ -179,7 +179,7 @@ function call_user_func_array__long_task($plain_title, $title, $hook, $args = nu
         if (($run_at_end_of_script) && (get_value('avoid_register_shutdown_function') !== '1')) {
             @ignore_user_abort(true); // Must keep going till completion
             register_shutdown_function(function () use ($plain_title, $title, $hook, $args, $force_immediate, $send_notification) {
-                // Workaround for Xdebug bug #1556 https://bugs.xdebug.org/view.php?id=1556
+                // Avoid specifying 'call_user_func_array__long_task' directly to workaround Xdebug bug #1556 https://bugs.xdebug.org/view.php?id=1556
                 call_user_func_array__long_task($plain_title, $title, $hook, $args, false, $force_immediate, $send_notification);
             });
             return new Tempcode();
