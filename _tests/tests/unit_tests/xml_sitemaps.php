915e3c52685214287a3099b25b939917f957a185
Chris Graham <chris@ocproducts.com>
2018-11-08 12:38:27 -0600

    Misc fixes

--- _tests/tests/unit_tests/xml_sitemaps.php
+++ _tests/tests/unit_tests/xml_sitemaps.php
@@ -45,27 +45,33 @@ class xml_sitemaps_test_set extends cms_test_case
     {
         $files = array('index.xml', 'set_0.xml');
         foreach ($files as $file) {
-            $url = 'http://ipullrank.com/tools/map-broker/index.php';
+            $c = file_get_contents(get_file_base() . '/data_custom/sitemaps/' . $file);
+
+            // Simple XML validation
+            require_code('xml');
+            new CMS_simple_xml_reader($c);
+
+            /* Bots apparently being blocked on here now
+            $url = 'https://ipullrank.com/tools/map-broker/index.php';
             $post_params = array(
                 'option' => '1',
                 'page' => 'go',
             );
 
             $tmp_file = get_file_base() . '/temp.xml';
-            $c = file_get_contents(get_file_base() . '/data_custom/sitemaps/' . $file);
             $cleaned_c = preg_replace('#https?://[^<>"]*#', 'http://example.com/', $c); // It checks URLs, which we don't want
             $cleaned_c = preg_replace('#(?U)(</url>.*</url>)(?-U).*</url>#s', '$1', $cleaned_c); // Strip down to speed up. Have 2 URLs else the validator is buggy
             file_put_contents($tmp_file, $cleaned_c);
 
             $files = array(
                 'xmlfile' => $tmp_file,
-                'option' => '1',
             );
             $result = http_download_file($url, null, true, false, 'Composr', $post_params, null, null, null, null, null, null, null, 6.0, false, $files);
 
             unlink($tmp_file);
 
             $this->assertTrue(strpos($result, '<strong>100%</strong> valid sitemap'), $result);
+            */
         }
     }
 }
