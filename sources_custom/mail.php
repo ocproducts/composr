39c2eff8c69687d055420e5c6ce08a012f025ef8
Chris Graham <chris@ocproducts.com>
2018-12-21 13:09:25 -0600

    Fix mailer bug, in some rare circumstances

--- sources_custom/mail.php
+++ sources_custom/mail.php
@@ -423,6 +423,7 @@ function mail_wrap($subject_line, $message_raw, $to_email = null, $to_name = nul
             $mime_type = get_mime_type(get_file_extension($filename), has_privilege($as, 'comcode_dangerous'));
 
             if ((strpos($path, '://') === false) && (substr($path, 0, 5) != 'gs://')) {
+                require_code('files2');
                 $real_attachment = array(
                     'mime' => $mime_type,
                     'filename' => $filename,
