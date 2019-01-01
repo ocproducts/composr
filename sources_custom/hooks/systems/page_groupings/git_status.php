d3f3de75a4e7969acd7db89c3c8d9c0978bb7c16
Chris Graham <chris@ocproducts.com>
2019-01-01 16:52:07 -0600

    Misc fixes

--- sources_custom/hooks/systems/page_groupings/git_status.php
+++ sources_custom/hooks/systems/page_groupings/git_status.php
@@ -28,7 +28,7 @@ class Hook_page_groupings_git_status
     public function run($member_id = null, $extensive_docs = false)
     {
         return array(
-            array('tools', 'status/notice', array('admin_git_status', array(), get_page_zone('admin_git_status')), 'Git status'),
+            array('tools', 'status/notice', array('admin_git_status', array(), get_page_zone('admin_git_status')), make_string_tempcode('Git status')),
         );
     }
 }
