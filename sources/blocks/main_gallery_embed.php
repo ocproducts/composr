5bbbe3a9609721d407e679a03284de3b0aac549e
Salman Abbas <salmanabbas007@gmail.com>
2019-01-31 05:33:23 +0500

    Block main_gallery_embed: cache_on syntax formatting

--- sources/blocks/main_gallery_embed.php
+++ sources/blocks/main_gallery_embed.php
@@ -49,7 +49,25 @@ class Block_main_gallery_embed
     public function caching_environment()
     {
         $info = array();
-        $info['cache_on'] = '(preg_match(\'#<\w+>#\',(array_key_exists(\'filter\',$map)?$map[\'filter\']:\'\').(array_key_exists(\'video_filter\',$map)?$map[\'video_filter\']:\'\'))!=0)?null:array(array_key_exists(\'as_guest\',$map)?($map[\'as_guest\']==\'1\'):false,get_param_integer($block_id.\'_max\',array_key_exists(\'max\',$map)?intval($map[\'max\']):null),get_param_integer($block_id.\'_start\',array_key_exists(\'start\',$map)?intval($map[\'start\']):0),((array_key_exists(\'pagination\',$map)?$map[\'pagination\']:\'0\')==\'1\'),((array_key_exists(\'root\',$map)) && ($map[\'root\']!=\'\'))?$map[\'root\']:get_param_string(\'keep_gallery_root\',null),array_key_exists(\'filter\',$map)?$map[\'filter\']:\'\',array_key_exists(\'video_filter\',$map)?$map[\'video_filter\']:\'\',array_key_exists(\'render_if_empty\',$map)?$map[\'render_if_empty\']:\'0\',array_key_exists(\'days\',$map)?$map[\'days\']:\'\',array_key_exists(\'sort\',$map)?$map[\'sort\']:\'add_date DESC\',get_param_integer(\'mge_start\',0),array_key_exists(\'param\',$map)?$map[\'param\']:db_get_first_id(),array_key_exists(\'zone\',$map)?$map[\'zone\']:\'\',(($map === null) || (!array_key_exists(\'select\',$map)))?\'*\':$map[\'select\'],(($map === null) || (!array_key_exists(\'video_select\',$map)))?\'*\':$map[\'video_select\'],array_key_exists(\'title\',$map)?$map[\'title\']:\'\',array_key_exists(\'check\',$map)?($map[\'check\']==\'1\'):true)';
+        $info['cache_on'] = <<<'PHP'
+            (preg_match('#<\w+>#', (array_key_exists('filter', $map) ? $map['filter'] : '') . (array_key_exists('video_filter', $map) ? $map['video_filter'] : '')) != 0) 
+                ? null : array(
+                    array_key_exists('as_guest', $map) ? ($map['as_guest'] == '1') : false, 
+                    get_param_integer($block_id . '_max', array_key_exists('max', $map) ? intval($map['max']) : null), 
+                    get_param_integer($block_id . '_start', array_key_exists('start', $map) ? intval($map['start']) : 0), 
+                    ((array_key_exists('pagination', $map) ? $map['pagination'] : '0') == '1'), 
+                    ((array_key_exists('root', $map)) && ($map['root'] != '')) ? $map['root'] : get_param_string('keep_gallery_root', null), 
+                    array_key_exists('filter', $map) ? $map['filter'] : '', array_key_exists('video_filter', $map) ? $map['video_filter'] : '', 
+                    array_key_exists('render_if_empty', $map) ? $map['render_if_empty'] : '0', array_key_exists('days', $map) ? $map['days'] : '', 
+                    array_key_exists('sort', $map) ? $map['sort'] : 'add_date DESC', get_param_integer('mge_start', 0),
+                    array_key_exists('param', $map) ? $map['param'] : db_get_first_id(), 
+                    array_key_exists('zone', $map) ? $map['zone'] : '', 
+                    (($map === null) || (!array_key_exists('select', $map))) ? '*' : $map['select'], 
+                    (($map === null) || (!array_key_exists('video_select', $map))) ? '*' : $map['video_select'], 
+                    array_key_exists('title', $map) ? $map['title'] : '', 
+                    array_key_exists('check', $map) ? ($map['check'] == '1') : true,
+                )
+PHP;
         $info['special_cache_flags'] = CACHE_AGAINST_DEFAULT | CACHE_AGAINST_PERMISSIVE_GROUPS;
         if (addon_installed('content_privacy')) {
             $info['special_cache_flags'] |= CACHE_AGAINST_MEMBER;
