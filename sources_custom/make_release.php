5e6b34024d067bb5d61b5bc54d9b537762d79913
Chris Graham <chris@ocproducts.com>
2019-03-06 13:27:18 -0600

    Whoops, typo

--- sources_custom/make_release.php
+++ sources_custom/make_release.php
@@ -149,7 +149,7 @@ function make_installers($skip_file_grab = false)
             \$SIZE_ARRAY = array({$size_list});
             \$FILE_ARRAY = array({$file_list});
             \$DATADOTCMS_FILE = @fopen('data.cms','rb');
-            if (]$DATADOTCMS_FILE === false) exit('data.cms missing / inaccessible -- make sure you upload it');
+            if (\$DATADOTCMS_FILE === false) exit('data.cms missing / inaccessible -- make sure you upload it');
             if (filesize('data.cms') != {$archive_size}) warn_exit('data.cms not fully uploaded, or wrong version for this installer');
             if (md5(file_array_get('{$md5_test_path}')) != '{$md5}') warn_exit('data.cms corrupt. Must not be uploaded in text mode');
 
