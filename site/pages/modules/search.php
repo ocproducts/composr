d4761eb5d4ce9cbffd3c9260ec09d438578ae8f2
Chris Graham <chris@ocproducts.com>
2019-03-20 15:53:51 -0500

    Extend search timeout, sometimes servers run slow

--- site/pages/modules/search.php
+++ site/pages/modules/search.php
@@ -674,7 +674,7 @@ class Module_search
                 $only_search_meta = get_param_integer('only_search_meta', 0) == 1;
                 $direction = get_param_string('direction', 'ASC');
                 if (php_function_allowed('set_time_limit')) {
-                    @set_time_limit(5); // Prevent errant search hooks (easily written!) taking down a server. Each call given 5 seconds (calling set_time_limit resets the timer).
+                    @set_time_limit(10); // Prevent errant search hooks (easily written!) taking down a server. Each call given 5 seconds (calling set_time_limit resets the timer).
                 }
                 $hook_results = $ob->run($content, $only_search_meta, $direction, $max, $start, $only_titles, $content_where, $author, $author_id, $cutoff, $sort, $max, $boolean_operator, $where_clause, $search_under, $boolean_search ? 1 : 0);
                 if (is_null($hook_results)) {
