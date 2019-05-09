ca419e777d593959e39e2bf6c1d4bc89a03193e6
Chris Graham <chris@ocproducts.com>
2019-05-08 20:49:03 -0500

    Fix early commit regarding URL to page link conversion (assumed changes that only exist in v11)

--- sources/site.php
+++ sources/site.php
@@ -700,13 +700,11 @@ function set_short_title($title)
  */
 function process_url_monikers($page, $redirect_if_non_canonical = true)
 {
-    if ($env_change) {
-        static $run_once = false;
-        if ($run_once) {
-            return;
-        }
-        $run_once = true;
+    static $run_once = false;
+    if ($run_once) {
+        return;
     }
+    $run_once = true;
 
     $zone = get_zone_name();
     $type = get_param_string('type', null, true);
