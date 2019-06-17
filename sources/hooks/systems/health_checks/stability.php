70a391cd46568a6b5f677f7ad6d77c4a93a5cc2f
Chris Graham <chris@ocproducts.com>
2019-06-17 11:43:44 -0500

    Fix Health Check error log size check

--- sources/hooks/systems/health_checks/stability.php
+++ sources/hooks/systems/health_checks/stability.php
@@ -71,7 +71,7 @@ class Hook_health_check_stability extends Hook_Health_Check
         }
 
         $path = get_custom_file_base() . '/data_custom/errorlog.php';
-        if (strlen($path) >= 25) {
+        if (filesize($path) >= 25) {
             $this->stateCheckManual('Check the Composr error log');
         }
 
